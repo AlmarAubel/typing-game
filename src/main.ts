6833b17e312512169af36ac098ecfd32aeb4651a
almaraubel <almar@aubel.nl>
2026-01-09 18:50:14 +0100

    refactor: Remove migration utilities and related code

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import { migrateToSharedStore } from "./utils/migrationHelper";
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -11,10 +10,4 @@ pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.use(router);
 
-// Perform migration after app initialization
 app.mount("#app");
-
-// Run migration in the next tick to ensure stores are ready
-setTimeout(() => {
-  migrateToSharedStore();
-}, 100);
